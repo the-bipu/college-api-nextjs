b5a5286e318ed77f0dac6a5b1e943183417b8b64
the-bipu <127449591+the-bipu@users.noreply.github.com>
2024-08-23 01:37:26 +0530

    create models and connection file

---
+++ models/College.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+const collegeSchema = new mongoose.Schema({
+    collegeCode: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+    collegeName: {
+        type: String,
+        required: true,
+    },
+}, {
+    timestamps: true,
+});
+
+export const College = mongoose.models.College || mongoose.model('College', collegeSchema);
